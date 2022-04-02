d0bc99034fafa586eee4581370ee5edd69d1cb7e
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-04-02 15:17:36 +0200

    changes in JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Coins from './components/Coins/Coins'
+import Contact from './components/Contact/Contact'
+import Header from './components/Header/Header'
+import Homepage from './components/Homepage/Homepage'
 
 const App = () => {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 bg-slate-50 px-12 py-8 flex justify-center md:justify-between'>
+            <div className='hidden md:block'><Link to='/' className=' font-bold text-xl text-orange-800'>Crypto Cafe</Link></div>
+            <div className='flex gap-4 text-xl'>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/coins'>Coins</CustomLink>
+                <CustomLink to='/contact'>Contact</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
